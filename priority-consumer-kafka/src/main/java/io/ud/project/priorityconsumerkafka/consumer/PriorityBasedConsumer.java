c7b0f92599aa2d60f8b7c6015779db5a3410b2e6
Udayabharathi Thiagarajan <udayabharathi.t@swiggy.in>
2021-11-24 16:10:53 +0530

    Changes.

--- priority-consumer-kafka/src/main/java/io/ud/project/priorityconsumerkafka/consumer/PriorityBasedConsumer.java
+++ priority-consumer-kafka/src/main/java/io/ud/project/priorityconsumerkafka/consumer/PriorityBasedConsumer.java
@@ -1,121 +1,93 @@
 package io.ud.project.priorityconsumerkafka.consumer;
 
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.env.Environment;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
+import java.util.function.Consumer;
 
 import javax.annotation.PostConstruct;
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 @Component
-@RequiredArgsConstructor
-@SuppressWarnings("unused")
 @Slf4j
 public class PriorityBasedConsumer {
-
-    private final Environment environment;
-
-    private final ThreadPoolTaskExecutor executor;
-
-    private final ApplicationContext context;
-
-    @Value("${spring.zookeeper.connect}")
-    private String zookeeperConnect;
 
     @Value("${spring.kafka.consumer.group-id}")
     private String consumerGroupId;
 
-    @Value("${kafka.topic.to.consume.topics}")
-    private List<String> topics;
-
-    @Value("${kafka.topic.consumer.threads}")
-    private Integer consumerThreads;
-
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    private SortedMap<String, KafkaConsumer<String, String>> topicVsKafkaConsumers;
-    private Map<String, Consumer> topicVsConsumerLogic;
+    private final List<TopicConsumer> consumersInPriorityOrder = new ArrayList<>();
 
-    public static final List<String> CONSUMED = new ArrayList<>();
+    @RequiredArgsConstructor(staticName = "of")
+    @Getter
+    private static class TopicConsumer {
+        private final String topic;
+        private final KafkaConsumer<String, String> kafkaConsumer;
+        private final Consumer<ConsumerRecords<String, String>> consumerLogic;
+    }
+
+    private void highPriorityConsumer(ConsumerRecords<String, String> records) {
+        // high priority processing...
+    }
+
+    private void mediumPriorityConsumer(ConsumerRecords<String, String> records) {
+        // medium priority processing...
+    }
+
+    private void lowPriorityConsumer(ConsumerRecords<String, String> records) {
+        // low priority processing...
+    }
 
     @PostConstruct
     public void init() {
-        Map<String, Integer> topicVsPriority = getTopicVsPriority();
-        topicVsKafkaConsumers = new TreeMap<>((topic1, topic2) ->
-                topicVsPriority.getOrDefault(topic2, 1) - topicVsPriority.getOrDefault(topic1, 1));
-        topicVsConsumerLogic = new HashMap<>();
-        for (String topic : topics) {
+        Map<String, Consumer<ConsumerRecords<String, String>>> topicVsConsumerLogic = new HashMap<>();
+        topicVsConsumerLogic.put("high_priority_queue", this::highPriorityConsumer);
+        topicVsConsumerLogic.put("medium_priority_queue", this::mediumPriorityConsumer);
+        topicVsConsumerLogic.put("low_priority_queue", this::lowPriorityConsumer);
+        // if you're taking the topic names from external configuration, make sure to order it based on priority.
+        for (String topic : Arrays.asList("high_priority_queue", "medium_priority_queue", "low_priority_queue")) {
             Properties consumerProperties = new Properties();
             consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
             consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
             consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
             consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
-            consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            // add other properties.
             KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProperties);
             consumer.subscribe(Collections.singletonList(topic));
-            topicVsKafkaConsumers.put(topic, consumer);
-            topicVsConsumerLogic.put(topic, getConsumer(topic));
+            consumersInPriorityOrder.add(TopicConsumer.of(topic, consumer, topicVsConsumerLogic.get(topic)));
         }
     }
 
-    @EventListener(ApplicationReadyEvent.class)
+    @EventListener(ApplicationReadyEvent.class) // To execute once the application is ready.
     @SneakyThrows
     public void startConsumers() {
-        int totalConsumed = 0;
-        Thread.sleep(100);
-        try {
-            while (totalConsumed < 50) {
-                for (Map.Entry<String, KafkaConsumer<String, String>> topicVsConsumer : topicVsKafkaConsumers.entrySet()) {
+        // For illustration purposes, I just wrote this synchronous code. Use thread pools where ever
+        // necessary for high performance.
+        while (true) { // poll infinitely
+            try {
+                // Consumers iterated based on priority.
+                for (TopicConsumer topicConsumer : consumersInPriorityOrder) {
                     ConsumerRecords<String, String> records
-                            = topicVsConsumer.getValue().poll(Duration.of(100, ChronoUnit.MILLIS));
-                    log.info("topic : {}, consumed: {}, totalConsumed: {}", topicVsConsumer.getKey(), records.count(), totalConsumed);
+                            = topicConsumer.getKafkaConsumer().poll(Duration.ofMillis(100));
                     if (!records.isEmpty()) {
-                        totalConsumed += records.count();
-                        topicVsConsumerLogic.getOrDefault(topicVsConsumer.getKey(), Consumer.DEFAULT).consume(records);
-                        break;
+                        topicConsumer.getConsumerLogic().accept(records);
+                        break;  // To start consuming again based on priority.
                     }
                 }
+            } catch (Exception e) {
+                // on any unknown runtime exceptions, ignoring here. You can add your proper logic.
+                log.error("Unknown exception occurred.", e);
             }
-            log.info("***********************");
-            for (String data : CONSUMED)
-                System.out.println(data);
-            log.info("***********************");
-        } finally {
-            if (!CollectionUtils.isEmpty(topicVsKafkaConsumers))
-                topicVsKafkaConsumers.values().forEach(KafkaConsumer::close);
-        }
-    }
-
-    private Map<String, Integer> getTopicVsPriority() {
-        Map<String, Integer> topicVsPriority = new HashMap<>();
-        for (String topic : topics) {
-            topicVsPriority.put(topic, environment.getProperty("kafka.topic."+topic+".priority", Integer.class, 1));
-        }
-        return topicVsPriority;
-    }
-
-    private Consumer getConsumer(String topic) {
-        try {
-            return context.getBean("consumer-" + topic, Consumer.class);
-        } catch (BeansException e) {
-            log.error("No consumer found for topic: {}", topic, e);
-            return Consumer.DEFAULT;
         }
     }
 }
